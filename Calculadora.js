9c2134a500d1ec2b211c62628dfda74cd92a2723
MMartincorena <martinmartincorena@gmail.com>
2022-03-18 00:01:59 -0300

    Calculadora funcional

---
+++ Calculadora.js
@@ -0,0 +1,32 @@
+const suma = (a,b) => {
+    return a + b;  
+}
+
+const resta = (a,b) => {
+    return a - b;
+}
+
+function rango(inicio, fin, valor) {
+    return (new Array(fin - inicio + 1)).fill(undefined).map((_) => value);
+}
+
+const multiplicacion = (a,b) => {
+    return rango(1,b,a)
+    .reduce((x,y) => suma(x,y))
+}
+
+const division = (numerador,denominador) => {
+    return rango(1,numerador, denominador)
+    .reduce((acumulador,numero) => {
+        if (multiplicacion(numero, denominador) <= denominador) {
+            return acumulador = acumulador + 1;
+        }
+        return acumulador;
+    })
+}
+
+
+console.log(suma(9,3));
+console.log(resta(7,6));
+console.log(multiplicacion(4,8)); // --> 40
+console.log(division(25, 5)) // --> 5
